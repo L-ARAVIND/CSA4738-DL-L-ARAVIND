10307cc0389babaca3a2a03bab8f0ef4041d6cf8
L-ARAVIND <192424080.simats@saveetha.com>
2026-04-29 14:12:18 +0530

    Update exp7_DL.py

--- exp7_DL.py
+++ exp7_DL.py
@@ -1,12 +1,16 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
-x = np.arange(-5,5,0.1)
+x = np.arange(-5, 5, 0.1)
+y = sigmoid(x)
 
-plt.plot(x, sigmoid(x))
-plt.title("Sigmoid Function")
+plt.plot(x, y, color='pink')
+
+plt.xlabel("Input")
+plt.ylabel("Output")
+plt.title("Visualization of the Sigmoid Function")
+
 plt.show()
